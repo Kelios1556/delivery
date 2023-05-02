f41cccefd6aeb37285de36a55b22f98e2d57227b
Kelios1556 <cey1556@connect.hku.hk>
2023-05-03 02:41:48 +0800

    second version

--- spinkler.py
+++ spinkler.py
@@ -4,14 +4,12 @@ import math
 from itertools import product, combinations
 length = 12 
 width = 6
-netRatio = 3
 # l_r = sprR / netRatio
 # w_r = sprR * width / length / netRatio
 netR = 1
 sprR = 4
-rr = math.ceil(sprR / netR)
+sprNetR = math.ceil(sprR / netR)
 P = 4
-r = [rr, rr, rr, rr]
 recL = math.ceil(length / netR)
 recW = math.ceil(width / netR)
 x_min = 0
@@ -27,27 +25,27 @@ def rec(x, y):
 def recFromPos(x, y):
     return math.floor((x - x_min) / netR), math.floor((y - y_min) / netR) 
 
-def sprinkler_range(x, y, r):
-    P_x_min = x[0] - r[0]
-    P_x_max = x[0] + r[0]
-    P_y_min = y[0] - r[0]
-    P_y_max = y[0] + r[0]
+def sprinkler_range(x, y):
+    P_x_min = x[0] - sprNetR 
+    P_x_max = x[0] + sprNetR
+    P_y_min = y[0] - sprNetR
+    P_y_max = y[0] + sprNetR
     for i in range(1, P):
-        if P_x_min > x[i] - r[i]: P_x_min = x[i] - r[i]
-        if P_x_max < x[i] + r[i]: P_x_max = x[i] + r[i]
-        if P_y_min > y[i] - r[i]: P_y_min = y[i] - r[i]
-        if P_y_max < y[i] + r[i]: P_y_max = y[i] + r[i]
+        if P_x_min > x[i] - sprNetR: P_x_min = x[i] - sprNetR
+        if P_x_max < x[i] + sprNetR: P_x_max = x[i] + sprNetR
+        if P_y_min > y[i] - sprNetR: P_y_min = y[i] - sprNetR
+        if P_y_max < y[i] + sprNetR: P_y_max = y[i] + sprNetR
     return P_x_min, P_x_max, P_y_min, P_y_max
 
-def N_(x, y, r, netW, netL): # N[y][x]
+def N_(x, y, netW, netL): # N[y][x]
     N = np.zeros((netW + 1, netL + 1), dtype=int)
     for i in range(P):
-        iirb, jjrb = rec(x[i] - r[i], y[i] - r[i])
-        iiru, jjru = rec(x[i] + r[i], y[i] + r[i])
+        iirb, jjrb = rec(x[i] - sprNetR, y[i] - sprNetR)
+        iiru, jjru = rec(x[i] + sprNetR, y[i] + sprNetR)
         xi_, yi_ = rec(x[i], y[i])
         for ii in range(iirb, iiru):
             for jj in range(jjrb, jjru):
-                if (dis(ii, jj, xi_, yi_) <= rr): N[jj][ii] += 1
+                if (dis(ii, jj, xi_, yi_) <= sprNetR): N[jj][ii] += 1
     return N.tolist()
 
 def enumerPos():
@@ -60,7 +58,7 @@ def wasteArea(x, y, N):
         for j in range(P_y_min, P_y_max):
             ii, jj = rec(i, j)
             result += N[jj][ii]
-    for i in range(recL, P_x_max + 1):
+    for i in range(recL, P_x_max):
         for j in range(P_y_min, P_y_max):
             ii, jj = rec(i, j)
             result += N[jj][ii]
@@ -88,6 +86,16 @@ def minM(N):
             sumV += (N[jj][ii] - meanN)**2
     return math.sqrt(sumV)
 
+def repeatRate(N):
+    repeat = 0
+    for i in range(recL):
+        for j in range(recW):
+            ii, jj = rec(i, j)
+            if N[jj][ii] > 1:
+                repeat += N[jj][ii] - 1
+    return repeat
+
+
 def coverRate(N):
     sumCover = 0
     for i in range(recL):
@@ -101,11 +109,11 @@ def coverRate(N):
 
 if __name__ == '__main__':
     pos = enumerPos()
-    w1 = 0.5; w2 = 0.5
+    w1 = 0.3; w2 = 0.3; w3 = 0.4
     minLoss = 999999999999
     for p in pos:
         x, y = map(list, zip(*p))
-        P_x_min, P_x_max, P_y_min, P_y_max = sprinkler_range(x, y, r)
+        P_x_min, P_x_max, P_y_min, P_y_max = sprinkler_range(x, y)
         x_min = min(P_x_min, 0)
         y_min = min(P_y_min, 0)
         x_max = max(P_x_max, recL)
@@ -113,9 +121,8 @@ if __name__ == '__main__':
 
         netL = x_max - x_min
         netW = y_max - y_min
-        N = N_(x, y, r, netW, netL)
-        if (coverRate(N) >= recL * recW):
-            loss = w1 * wasteArea(x, y, N) + w2 * minM(N)
-            if (loss < minLoss):
-                minLoss = loss
-                print(list(zip(x, y)))
+        N = N_(x, y, netW, netL)
+        loss = w1 * wasteArea(x, y, N) + w2 * repeatRate(N) + w3 * (recL * recW - coverRate(N))
+        if (loss < minLoss):
+            minLoss = loss
+            print(list(zip(x, y)))
